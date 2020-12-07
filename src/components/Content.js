e5c8da6d9451ff127cd00322a6c40a124398d471
Mari Rødseth Haugen <h_mari@hotmail.com>
2020-12-07 14:54:22 +0100

    started on flag

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,74 +1,76 @@
-import React ,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Meme.css";
-import {faCommentDots,faFire, faSnowflake } from "@fortawesome/free-solid-svg-icons"
+import { faCommentDots, faFire, faSnowflake, faFlag } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import memeFacade from "../facades/memeFacade";
 
 
 export default function Content({ meme, hasVotes, isLoggedIn }) {
-    const [msg, setMsg] = useState("");
-    const [voteType, setVoteType] = useState("none");
-    const [votes, setVotes] = useState(meme);
-    const [commentCounter, setCommentCounter] = useState(0);
-    let upvotedMemes = JSON.parse(localStorage.getItem("upvotedMemes"));
-    let downvotedMemes = JSON.parse(localStorage.getItem("downvotedMemes"));
-    let username = localStorage.getItem("user");
-    
-    useEffect(() => {
-      checkUpvotedMemes();
-      if (hasVotes) {
+  const [msg, setMsg] = useState("");
+  const [voteType, setVoteType] = useState("none");
+  const [votes, setVotes] = useState(meme);
+  const [commentCounter, setCommentCounter] = useState(0);
+  const [showReport, setShowReport] = useState(false);
+  const handleShowReport = () => setShowReport(!showReport);
+  let upvotedMemes = JSON.parse(localStorage.getItem("upvotedMemes"));
+  let downvotedMemes = JSON.parse(localStorage.getItem("downvotedMemes"));
+  let username = localStorage.getItem("user");
+
+  useEffect(() => {
+    checkUpvotedMemes();
+    if (hasVotes) {
       memeFacade.getComments(meme.meme_id)
-      .then(comments => setCommentCounter(comments.length))
-      }
-    }, []);
+        .then(comments => setCommentCounter(comments.length))
+    }
+  }, []);
 
 
-    const vote = (e) => {
-      let type = e.currentTarget.id
-  
-      if (type === "up") {
-        if (voteType === "up") {
-          setMsg("");
-          setVoteType("none");
-        } else {
-          if (!hasVotes) {
-            setMsg("Upvoted")
-          }
-          setVoteType(type);
+  const vote = (e) => {
+    let type = e.currentTarget.id
+
+    if (type === "up") {
+      if (voteType === "up") {
+        setMsg("");
+        setVoteType("none");
+      } else {
+        if (!hasVotes) {
+          setMsg("Upvoted")
         }
+        setVoteType(type);
+      }
 
-        memeFacade.upvoteMeme(username, meme)
+      memeFacade.upvoteMeme(username, meme)
         .then(res => {
-          setVotes({ ...res})
-          upvotedMemes.push({...res});
+          setVotes({ ...res })
+          upvotedMemes.push({ ...res });
           localStorage.setItem("upvotedMemes", JSON.stringify(upvotedMemes));
         })
-        .catch(err => printError(err, setMsg)); 
+        .catch(err => printError(err, setMsg));
 
-      } else if (type === "down") {
-        if (voteType === "down") {
-          setMsg("");
-          setVoteType("none");
-        } else {
-          if (!hasVotes) {
-            setMsg("Downvoted")
-          }
-          setVoteType(type);
+    } else if (type === "down") {
+      if (voteType === "down") {
+        setMsg("");
+        setVoteType("none");
+      } else {
+        if (!hasVotes) {
+          setMsg("Downvoted")
         }
-        
-        memeFacade.downvoteMeme(username, meme)
+        setVoteType(type);
+      }
+
+      memeFacade.downvoteMeme(username, meme)
         .then(res => {
-          setVotes({ ...res});
-          downvotedMemes.push({...res});
+          setVotes({ ...res });
+          downvotedMemes.push({ ...res });
           localStorage.setItem("downvotedMemes", JSON.stringify(downvotedMemes));
         })
-        .catch(err => printError(err, setMsg));    
-      }
+        .catch(err => printError(err, setMsg));
     }
+  }
 
-    const checkUpvotedMemes = () => {
-      if (upvotedMemes !== null && downvotedMemes !== null) {
+  const checkUpvotedMemes = () => {
+    if (upvotedMemes !== null && downvotedMemes !== null) {
       upvotedMemes.find(upvotedMeme => {
         if (upvotedMeme.imageUrl === meme.imageUrl) {
           setVoteType("up");
@@ -82,58 +84,64 @@ export default function Content({ meme, hasVotes, isLoggedIn }) {
         return 0;
       });
     }
-    }
-
-     return (
-        <div className="content">
-          
-          <img className="meme-img" src={meme.imageUrl} alt="" />
-          <FontAwesomeIcon
-            id="up"
-            onClick={vote}
-            className="voteBtn"
-            icon={faFire}
-            style={voteType === "up" ? { color: "red" } : { color: "black" }}
-            size="2x" />
-            {hasVotes ? (
-              <p className="counter">{votes.upvotes}</p>
-            ) : ""}
-          <FontAwesomeIcon
-            id="down"
-            onClick={vote}
-            className="voteBtn"
-            icon={faSnowflake}
-            style={voteType === "down" ? { color: "lightblue" } : { color: "black" }}
-            size="2x" />         
-            {hasVotes ? (
-                    <p className="counter">{votes.downvotes}</p>
-                  ) : ""}
-                <p className="voteText" style={!isLoggedIn ? {color: "red"} : {color: "black"}}>
-                  {msg}
-                </p>
-                <br />
-                
-            {hasVotes && (
-            <div>
-            <p className="counter" 
-            style={{float: "right", marginLeft: "8px", marginTop: "10px"}}>
-              {commentCounter}
-            </p>
-            <Link to={`/comment/${meme.meme_id}`}> 
-            <FontAwesomeIcon
-             size="2x"
-             icon={faCommentDots} 
-             style={{color: "black", float: "right", marginTop: "10px"}}
-              />
-            </Link>
-            </div>
-            )}
-          <br />
-          
-       </div> 
-      )
-     
   }
+
+  return (
+    <div className="content">
+
+      <img className="meme-img" src={meme.imageUrl} alt="" />
+      <FontAwesomeIcon
+        id="up"
+        onClick={vote}
+        className="voteBtn"
+        icon={faFire}
+        style={voteType === "up" ? { color: "red" } : { color: "black" }}
+        size="2x" />
+      {hasVotes ? (
+        <p className="counter">{votes.upvotes}</p>
+      ) : ""}
+      <FontAwesomeIcon
+        id="down"
+        onClick={vote}
+        className="voteBtn"
+        icon={faSnowflake}
+        style={voteType === "down" ? { color: "lightblue" } : { color: "black" }}
+        size="2x" />
+      {hasVotes ? (
+        <p className="counter">{votes.downvotes}</p>
+      ) : ""}
+      <p className="voteText" style={!isLoggedIn ? { color: "red" } : { color: "black" }}>
+        {msg}
+      </p>
+      <br />
+
+      {hasVotes && (
+        <div>
+          <p className="counter"
+            style={{ float: "right", marginLeft: "8px", marginTop: "10px" }}>
+            {commentCounter}
+          </p>
+          <Link to={`/comment/${meme.meme_id}`}>
+            <FontAwesomeIcon
+              size="2x"
+              icon={faCommentDots}
+              style={{ color: "black", float: "right", marginTop: "10px" }}
+            />
+          </Link>
+          <FontAwesomeIcon
+            onClick={handleShowReport}
+            size="2x"
+            icon={faFlag}
+            style={{ color: "black", float: "right", marginTop: "10px", marginRight: "10px" }}
+          />
+        </div>
+      )}
+      <br />
+
+    </div>
+  )
+
+}
 
 const printError = (promise, setError) => {
   promise.fullError.then(function (status) {
